2d01ebfa70b4ad7e1e8aca30a1ccf5d246558a85
isaias005 <isaias005@hotmail.com>
2023-06-07 01:55:39 -0400

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+			dark: '#121212',
+			gray: '#252525',
+			'light-gray': '#333333',
+			'lighter-gray': '#555555',
+			red: '#FF3B3B',
+			purple: '#731DFF',
+			yellow: '#FFF500',
+			black: '#000000',
+			white: '#FCFCFC',
+			transparent: 'transparent',
+		},
+		extend: {
+			
+		},
+	},
+	plugins: [],
+}
